bf7787dd7e2a1909a217e12fc9a5684bb559fb92
Alexander Silver <asilver7493@gmail.com>
2020-03-05 02:31:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,14 +6,14 @@ def summon_captain_planet(src)
   src.map{|n| n= n.capitalize +"!"}
 end
 
-def long_planeteer_calls(src)
-  src.each do |ele|
-    if ele.length >4
-      return true
-    end
-  end
-  return false
-end
+#def long_planeteer_calls(src)
+  #src.each do |ele|
+    #if ele.length >4
+    #  return true
+   # end
+  #end
+ # return false
+#end
 
 #def find_the_cheese(src)
  # cheese_types = ["cheddar", "gouda", "camembert"]
