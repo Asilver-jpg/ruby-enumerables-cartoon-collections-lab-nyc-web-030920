fb5da8bf0a590f22805276adc5d5473aa123618e
Alexander Silver <asilver7493@gmail.com>
2020-03-05 02:28:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(src)
+  src.each_with_index {|ele,i| puts ""#{i} #{ele}}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(src)
+  src.map{|n| n= n.capitalize +"!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(src)
+  src.each do |ele|
+    if ele.length >4
+      return true
+    end
+  end
+  return false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(src)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
